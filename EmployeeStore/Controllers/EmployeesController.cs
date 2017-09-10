3ad0831b500e1565ebe4275eb1cc4f565d0e514f
Pankaj Vashishta <pankaj.vashishta@gmail.com>
2017-09-10 21:16:09 +1000

    BP-0017 : Added data Access layer

---
+++ EmployeeStore/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeesDataAccess;
+
+namespace EmployeeStore.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmplyeeDBEntities entities = new EmplyeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmplyeeDBEntities entities = new EmplyeeDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
