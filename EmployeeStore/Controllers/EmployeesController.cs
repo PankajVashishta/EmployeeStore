e17e720273a8bf1e2e070a3b9f9ad8fbe07c28fc
Pankaj Vashishta <pankaj.vashishta@gmail.com>
2017-09-17 02:38:05 +1000

    BP-0018 : Created Put, Post and Delete HTTP verbs

--- EmployeeStore/Controllers/EmployeesController.cs
+++ EmployeeStore/Controllers/EmployeesController.cs
@@ -10,7 +10,8 @@ namespace EmployeeStore.Controllers
 {
     public class EmployeesController : ApiController
     {
-        public IEnumerable<Employee> Get()
+        [HttpGet]
+        public IEnumerable<Employee> LoadSoemthing()
         {
             using (EmplyeeDBEntities entities = new EmplyeeDBEntities())
             {
@@ -18,11 +19,92 @@ namespace EmployeeStore.Controllers
             }
         }
 
-        public Employee Get(int id)
+        [HttpGet]
+        public HttpResponseMessage LoadSoemthing(int id)
         {
             using (EmplyeeDBEntities entities = new EmplyeeDBEntities())
             {
-                return entities.Employees.FirstOrDefault(e => e.ID == id);
+                var entity =  entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                return entity != null ?
+                    Request.CreateResponse(HttpStatusCode.OK, entity) :
+                    Request.CreateErrorResponse(HttpStatusCode.NotFound, $"Employee with id {id} is not found");
+            }
+        }
+
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmplyeeDBEntities entities = new EmplyeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri , employee.ID.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (EmplyeeDBEntities entities = new EmplyeeDBEntities())
+                {
+
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (entity != null)
+                    {
+                        entities.Employees.Remove(entities.Employees.FirstOrDefault(e => e.ID == id));
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound, $"Employee with id {id} is not found");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmplyeeDBEntities entities = new EmplyeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (entity != null)
+                    {
+                        entity.FirstName = employee.FirstName;
+                        entity.LastName = employee.LastName;
+                        entity.Salary = employee.Salary;
+                        entity.Gender = employee.Gender;
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                    else
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound, $"Employee with id {id} is not found");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
         }
     }
